57d0f0f57d860ba756688ae02552ba41ac29f346
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-27 18:15:12 -0300

    Add Button para Login com Github

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -2,14 +2,26 @@
 import { signIn } from "@/auth"
 
 export default function SignIn() {
-  return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
-  )
-} 
+    return (
+        <>
+            <form
+                action={async () => {
+                    "use server"
+                    await signIn("google")
+                }}
+            >
+                <button className="bg-fuchsia-400" type="submit">Signin with Google</button>
+            </form>
+            <form
+                action={async () => {
+                    "use server";
+                    await signIn("github");
+                }}
+            >
+                <button className="bg-emerald-400" type="submit">Sign in with GitHub</button>
+            </form>
+        </>
+
+
+    )
+}
