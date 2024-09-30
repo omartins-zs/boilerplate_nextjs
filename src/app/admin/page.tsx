884592b235131c4ec33a092acd8a18da6effb4d4
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-30 11:30:07 -0300

    Add Validação de Session e Mensagem de Não Logado

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,19 +1,37 @@
-'use client'
+"use client";
 
-import { useSession } from "next-auth/react"
-import LogoutButton from "@/components/LogoutButton"
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import LogoutButton from "@/components/LogoutButton";
 
 export default function AdminPage() {
     const { data: session } = useSession();
 
+    useEffect(() => {
+        if (!session) {
+            const timer = setTimeout(() => {
+                window.location.href = "/login";
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [session]);
+
     if (!session) {
-        return <h1>Acesso Negado. Faça login para continuar.</h1>; // Mensagem para usuários não autenticados
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong className="font-bold">Acesso Negado!</strong>
+                    <span className="block sm:inline"> Faça login para continuar.</span>
+                </div>
+            </div>
+        );
     }
 
     return (
         <div>
-            <h1>Bem-vindo, {session.user?.name}!</h1>
-            <LogoutButton /> {/* Botão de logout */}
+            <h1 className="text-3xl font-bold">Bem-vindo, {session.user?.name}!</h1>
+            <LogoutButton />
         </div>
     );
 }
