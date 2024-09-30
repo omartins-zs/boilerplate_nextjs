5982266175ada535b89528ad17bbc182dd393ec3
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-30 11:15:22 -0300

    Add Login pelo Provider Credentials

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,57 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
 import Discord from "next-auth/providers/discord"
-
+import Credentials from "next-auth/providers/credentials"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google, GitHub, Discord],
-})
+    providers: [
+        Google,
+        GitHub,
+        Discord,
+        Credentials({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+
+                console.log("Credentials", credentials);
+
+                // Verifica credenciais estáticas
+                if (!credentials) {
+                    return null;
+                }
+
+                if (credentials.email === "gabriel@email.com" && credentials.password === "123") {
+                    return {
+                        id: "1",
+                        name: "Gabriel Matheus",
+                        email: "gabriel@email.com",
+                    };
+                }
+
+                // Se as credenciais não corresponderem, tenta verificar no banco de dados
+                // let user = null;
+
+                // // lógica para salgar e hashear a senha
+                // const pwHash = saltAndHashPassword(credentials.password);
+
+                // // lógica para verificar se o usuário existe
+                // user = await getUserFromDb(credentials.email, pwHash);
+
+                // if (!user) {
+                //     // Nenhum usuário encontrado
+                //     throw new Error("User not found.");
+                // }
+
+                // // Retorna o objeto do usuário com seus dados de perfil
+                // return user;
+            },
+        }),
+    ],
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            return baseUrl + "/admin"; // Redireciona para a página admin após login
+        },
+    },
+});
