43b804bbc23e44df10333913a41bd9af6a35cac9
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-30 11:28:13 -0300

    Add Button de Logout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 
 import { useSession } from "next-auth/react"
+import LogoutButton from "@/components/LogoutButton"
 
 export default function AdminPage() {
-  const { data: session, status } = useSession(); // Obtém a sessão
+    const { data: session } = useSession();
 
-  if (status === "loading") {
-    return <h1>Loading...</h1>; // Exibe uma mensagem enquanto a sessão está sendo carregada
-  }
+    if (!session) {
+        return <h1>Acesso Negado. Faça login para continuar.</h1>; // Mensagem para usuários não autenticados
+    }
 
-  if (!session) {
-    return <h1>Acesso negado. Faça login para continuar.</h1>; // Mensagem caso não esteja logado
-  }
-
-  return <h1>Bem-vindo,  {session.user?.name}!</h1>;
+    return (
+        <div>
+            <h1>Bem-vindo, {session.user?.name}!</h1>
+            <LogoutButton /> {/* Botão de logout */}
+        </div>
+    );
 }
