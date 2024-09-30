16c02211276194b199f0237eb0a44c3f4800a12c
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-30 17:49:54 -0300

    Criando page para Registro de Usuario

---
+++ src/app/register/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function SignUpForm() {
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    setError("");
+    setSuccess("");
+
+    const res = await fetch("actions/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setSuccess("Usuário cadastrado com sucesso!");
+      router.push("/login");
+    } else {
+      setError(data.error || "Ocorreu um erro durante o cadastro.");
+    }
+  };
+
+  return (
+    <div className="mx-auto max-w-sm space-y-8">
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl font-bold">Cadastre-se</h1>
+      </div>
+
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="name">Nome</Label>
+          <Input
+            id="name"
+            placeholder="Seu nome"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            placeholder="m@example.com"
+            required
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="password">Senha</Label>
+          <Input
+            id="password"
+            required
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        {error && <div className="text-red-500">{error}</div>} {/* Exibe mensagem de erro */}
+        {success && <div className="text-green-500">{success}</div>} {/* Exibe mensagem de sucesso */}
+
+        <Button className="w-full" type="submit">
+          Cadastrar
+        </Button>
+      </form>
+    </div>
+  );
+}
