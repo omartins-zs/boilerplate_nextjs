519db70d74b1e2ecc699e3c8f4de27c361d78bae
omartins-zs <gabrielmatheus209@gmail.com>
2024-09-30 09:49:35 -0300

    Add Provider do Discord

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
+import Discord from "next-auth/providers/discord"
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google, GitHub],
+    providers: [Google, GitHub, Discord],
 })
