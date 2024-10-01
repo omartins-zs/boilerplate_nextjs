22506fee4c91f1b984b249da0beef2cda790555f
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-01 16:07:06 -0300

    Importando o component de Form para Cadastro User

---
+++ src/components/sign-up.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/app/register/page";
+
+export default function SignUp() {
+  return (
+    <main className="flex flex-col items-center gap-8">
+      <SignUpForm />
+    </main>
+  );
+}
