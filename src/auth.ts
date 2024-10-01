b0c532b14f7dda5713d384b172f317d977faa72e
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-01 13:26:52 -0300

    Fazendo Login Estatico com Credentials

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,8 @@ import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
 import Discord from "next-auth/providers/discord"
 import Credentials from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { Prisma } from "@prisma/client"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(Prisma),
     providers: [
         Google,
         GitHub,
@@ -18,14 +15,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 password: {},
             },
             authorize: async (credentials) => {
-
-                console.log("Credentials", credentials);
-
-                // Verifica credenciais estáticas
-                if (!credentials) {
-                    return null;
-                }
-
                 if (credentials.email === "gabriel@email.com" && credentials.password === "123") {
                     return {
                         id: "1",
@@ -33,29 +22,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                         email: "gabriel@email.com",
                     };
                 }
-
-                // Se as credenciais não corresponderem, tenta verificar no banco de dados
-                // let user = null;
-
-                // // lógica para salgar e hashear a senha
-                // const pwHash = saltAndHashPassword(credentials.password);
-
-                // // lógica para verificar se o usuário existe
-                // user = await getUserFromDb(credentials.email, pwHash);
-
-                // if (!user) {
-                //     // Nenhum usuário encontrado
-                //     throw new Error("User not found.");
-                // }
-
-                // // Retorna o objeto do usuário com seus dados de perfil
-                // return user;
+                return null;
             },
         }),
     ],
     callbacks: {
         async redirect({ url, baseUrl }) {
-            return baseUrl + "/admin"; // Redireciona para a página admin após login
+            return baseUrl + "/admin";
+        },
+        async jwt({ token }) {
+            return token
+        },
+        async session({ session }) {
+            return session;
         },
     },
 });
