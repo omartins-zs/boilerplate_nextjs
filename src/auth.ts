5f3fed90370c85c4b0083f28b9da06cafe74b577
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-01 14:02:35 -0300

    Fazendo Login com Prisma e Bd

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,12 @@ import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
 import Discord from "next-auth/providers/discord"
 import Credentials from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: "jwt" },
     providers: [
         Google,
         GitHub,
@@ -15,19 +19,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 password: {},
             },
             authorize: async (credentials) => {
-                if (credentials.email === "gabriel@email.com" && credentials.password === "123") {
-                    return {
-                        id: "1",
-                        name: "Gabriel Matheus",
-                        email: "gabriel@email.com",
-                    };
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: credentials.email as string
+                    }
+                })
+                console.log("Usuario ", user)
+                if (user) {
+                    return user;
                 }
                 return null;
+                // if (credentials.email === "gabriel@email.com" && credentials.password === "123") {
+                //     return {
+                //         id: "1",
+                //         name: "Gabriel Matheus",
+                //         email: "gabriel@email.com",
+                //     };
+                // }
+                // return null;
             },
         }),
     ],
     callbacks: {
-        async redirect({ url, baseUrl }) {
+        async redirect({ baseUrl }) {
             return baseUrl + "/admin";
         },
         async jwt({ token }) {
